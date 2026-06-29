d1983f564f01ee427100a641af582ef03d9936f9
Vinay Daripelly <152810364+Vinay-Daripelly@users.noreply.github.com>
2026-06-29 21:37:25 +0530

    Clear apiKey and orgmail constants
    Removed values for apiKey and orgmail constants.

--- app/src/main/java/com/example/ecocraftplastic/Constants.kt
+++ app/src/main/java/com/example/ecocraftplastic/Constants.kt
@@ -1,4 +1,4 @@
 object Constants {
-   const val apiKey = "AIzaSyCw3eXo9aNbJJI83zHye6AYBwqcE6DqoFI"
-   const val orgmail="agrigeninnovators@gmail.com"
+   const val apiKey = 
+   const val orgmail=
 }
